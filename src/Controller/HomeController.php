9dcff1835d1a5462a371bec900ee7939c3cd318d
Rodrigo Gomes <cklrod@hotmail.com>
2023-09-02 17:08:51 -0300

    criando controller e pagina category, organizando menu de navegação e titulo da pagina conforme a categoria / Creating Controller and Category Page, Organizing Navigation Menu and Page Title According to Category

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public $categories = [
+        ['title' => 'Mundo', 'text' => 'Notícias sobre o Mundo'],
+        ['title' => 'Brasil', 'text' => 'Notícias sobre o Brasil'],
+        ['title' => 'Tecnologia', 'text' => 'Notícias sobre Tecnologia'],
+        ['title' => 'Design', 'text' => 'Notícias sobre Design'],
+        ['title' => 'Cultura', 'text' => 'Notícias sobre Cultura'],
+        ['title' => 'Negócios', 'text' => 'Notícias sobre Negócios'],
+        ['title' => 'Política', 'text' => 'Notícias sobre Política'],
+        ['title' => 'Opinião', 'text' => 'Notícias sobre Opinião'],
+        ['title' => 'Ciência', 'text' => 'Notícias sobre Ciência'],
+        ['title' => 'Saúde', 'text' => 'Notícias sobre Saúde'],
+        ['title' => 'Estilo', 'text' => 'Notícias sobre Estilo de Vida'],
+        ['title' => 'Viagens', 'text' => 'Notícias sobre Viagens'],
+    ];
+
     #[Route('/', name: 'app_home')]
-    public function new(): Response
+    public function index(): Response
     {
+
+        $pageTitle = 'Sistema de Notícias';
+
         return $this->render('home/index.html.twig', [
+            'pageTitle' => $pageTitle,
+            'categories' => $this->categories
+        ]);
+    }
+
+    #[Route('/categoria/{slug}', name: 'app_category')]
+    public function category(string $slug = null): Response
+    {
+        $pageTitle = $slug;
+
+        return $this->render('home/category.html.twig', [
+            'pageTitle' => $pageTitle,
+            'categories' => $this->categories
         ]);
     }
 }
